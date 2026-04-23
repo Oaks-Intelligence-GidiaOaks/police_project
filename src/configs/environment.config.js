4124df55ea4823d204c34c5e9b9e2808dc2ee38a
Uduak Edet <udispecial01@gmail.com>
2026-04-23 12:07:59 +0100

    Update production API base URL

--- src/configs/environment.config.js
+++ src/configs/environment.config.js
@@ -2,7 +2,7 @@
 const environment = {
   production: {
     API_BASE_URL:
-      "https://police-project-api-c0b1388972f6.herokuapp.com/api/v1",
+      "https://police-project-api.onrender.com/api/v1",
   },
   development: {
     API_BASE_URL: "http://localhost:5000/api/v1/",
